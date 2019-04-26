e6a85de57081b24934282c43a12b022f9f740cbe
Guest2 <guest2@10-253-107-15.dynapool.nyu.edu>
2019-04-26 17:42:03 -0400

    added location services

--- maptest/ViewController.swift
+++ maptest/ViewController.swift
@@ -8,21 +8,56 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-    let regionRadius: CLLocationDistance = 500;
-    func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius);
-        mapView.setRegion(coordinateRegion, animated: true);
-    }
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager();
     
     @IBOutlet weak var mapView: MKMapView!;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let initialLocation = CLLocation(latitude: 40.7391728, longitude: -73.9831523);
-        centerMapOnLocation(location: initialLocation);
+        let dummyLocation = CLLocation(latitude: 40.7391728, longitude: -73.9831523);
+        
+        // ask user for permission to always use location
+        locationManager.requestAlwaysAuthorization();
+        locationManager.requestWhenInUseAuthorization();
+        // if location services are allowed, start getting user location
+        if (CLLocationManager.locationServicesEnabled()) {
+            // set delegate, accuracy
+            locationManager.delegate = self;
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+            
+            locationManager.startUpdatingLocation();
+        }
+        
     }
+    
+    // https://developer.apple.com/documentation/corelocation/cllocationmanagerdelegate/1423615-locationmanager
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        /*
+        manager: "location manager obj that generated the update event"
+        locationsarray of CLLocations. always contains at least 1 element representing current location
+            if updates were deferred or multiple locations arrived before they could be delivered, array contains
+            additional entries (in order they occurred. most recent at the end).
+        */
+        let mostRecentLocationIndex = locations.count;
+        let mostRecentLocation  = locations[mostRecentLocationIndex - 1];
+        NSLog("location found");
+        centerOnLocation(location: mostRecentLocation);
+    }
+    
+    // center map at given coordinates
+    func centerOnLocation(location: CLLocation) {
+        let regionRadius: CLLocationDistance = 500;
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius);
+        mapView.setRegion(coordinateRegion, animated: true);
+    }
+    
+    // draw markers for two locations, and center map between them
+    func showTwoPoints(location1: CLLocation, location2: CLLocation) {
+        
+    }
+    
 
 }
