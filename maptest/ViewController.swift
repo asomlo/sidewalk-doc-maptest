38d5036305df0f8e68f85ebf2648187fdbe402c6
Guest2 <guest2@10-253-111-198.dynapool.nyu.edu>
2019-04-28 16:08:59 -0400

    added simulated doctor marker

--- maptest/ViewController.swift
+++ maptest/ViewController.swift
@@ -12,12 +12,19 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     let locationManager = CLLocationManager();
+    // when redrawing map for updated doctor location, use last known user location for other pt
+    
+    // userLocation variable not needed, can use mapView.UserLocation();
+    //var lastKnownUserLocation = CLLocation(latitude: 0.0, longitude: 0.0);
+    var lastKnownDocLocation = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0);
+    var activeAlert = false;
+    var docMarker = MKPointAnnotation();
     
     @IBOutlet weak var mapView: MKMapView!;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let dummyLocation = CLLocation(latitude: 40.7391728, longitude: -73.9831523);
+        // let dummyLocation = CLLocation(latitude: 40.7391728, longitude: -73.9831523);
         
         // ask user for permission to always use location
         locationManager.requestAlwaysAuthorization();
@@ -27,9 +34,41 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             // set delegate, accuracy
             locationManager.delegate = self;
             locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
-            
             locationManager.startUpdatingLocation();
         }
+        mapView.showsUserLocation = true;
+        mapView.showsPointsOfInterest = false;
+        // title does not seem to move when coordinate changes. removing it i find out how to move them both
+        //marker.title = "Doc";
+        
+        
+        // simulating a moving doctor
+        // source on delayed execution code: https://stackoverflow.com/questions/28821722/delaying-function-in-swift
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute: {
+            self.addDoctorMarker();
+            self.lastKnownDocLocation.latitude = 40.740447;
+            self.lastKnownDocLocation.longitude = -73.981132;
+            self.updateDoctorLoc(docLocation: self.lastKnownDocLocation);
+        });
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0, execute: {
+            self.lastKnownDocLocation.latitude = 40.740512;
+            self.lastKnownDocLocation.longitude = -73.981883;
+            self.updateDoctorLoc(docLocation: self.lastKnownDocLocation);
+        });
+        DispatchQueue.main.asyncAfter(deadline: .now() + 15.0, execute: {
+            self.lastKnownDocLocation.latitude = 40.739939;
+            self.lastKnownDocLocation.longitude = -73.982479;
+            self.updateDoctorLoc(docLocation: self.lastKnownDocLocation);
+        });
+        DispatchQueue.main.asyncAfter(deadline: .now() + 20.0, execute: {
+            self.lastKnownDocLocation.latitude = 40.739321;
+            self.lastKnownDocLocation.longitude = -73.982908;
+            self.updateDoctorLoc(docLocation: self.lastKnownDocLocation);
+        });
+        // alert ends, remove doctor marker
+        DispatchQueue.main.asyncAfter(deadline: .now() + 25.0, execute: {
+            self.removeDoctorMarker();
+        });
         
     }
     
@@ -37,27 +76,57 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         /*
         manager: "location manager obj that generated the update event"
-        locationsarray of CLLocations. always contains at least 1 element representing current location
+         locations: array of CLLocations. always contains at least 1 element representing current location.
             if updates were deferred or multiple locations arrived before they could be delivered, array contains
             additional entries (in order they occurred. most recent at the end).
         */
-        let mostRecentLocationIndex = locations.count;
-        let mostRecentLocation  = locations[mostRecentLocationIndex - 1];
-        NSLog("location found");
-        centerOnLocation(location: mostRecentLocation);
+        //let mostRecentLocationIndex = locations.count - 1;
+        //lastKnownUserLocation = locations[mostRecentLocationIndex];
+        
+        // when updated user location is received:
+            // if there is not a doctor on the way (AKA no active alert), recenter map on new user location
+            // if there is an active alert/doctor, recenter map between doctor info and new user info
+        if (!activeAlert) {
+            centerOnLocation(location: mapView.userLocation.coordinate);
+        } else {
+            updateDoctorLoc(docLocation: lastKnownDocLocation);
+        }
+
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        NSLog(error.localizedDescription);
     }
     
-    // center map at given coordinates
-    func centerOnLocation(location: CLLocation) {
+    
+    // center map on given loction
+    func centerOnLocation(location: CLLocationCoordinate2D) {
+        // when only showing the user, use default region radius of 500m.
+        //let regionRadius = CLLocationDistance(exactly: 500);
         let regionRadius: CLLocationDistance = 500;
-        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius);
+        let coordinateRegion = MKCoordinateRegion(center: location, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius);
         mapView.setRegion(coordinateRegion, animated: true);
     }
+
+    func addDoctorMarker() {
+        activeAlert = true;
+        mapView.addAnnotation(docMarker);
+    }
+    func removeDoctorMarker() {
+        activeAlert = false;
+        mapView.removeAnnotation(docMarker);
+    }
     
-    // draw markers for two locations, and center map between them
-    func showTwoPoints(location1: CLLocation, location2: CLLocation) {
+    // center map between user and doctor locations, show marker on doc location
+    func updateDoctorLoc(docLocation: CLLocationCoordinate2D) {
+        // find distance between user and given doctor location to establish regionRadius
+            // (creating CLLocations since CLLocationCoordinate2D doesn't have distance function)
+        let distanceBetweenCoordinates = CLLocation(latitude: mapView.userLocation.coordinate.latitude, longitude: mapView.userLocation.coordinate.longitude).distance(from: CLLocation(latitude: docLocation.latitude, longitude: docLocation.longitude));
+        // set zoom distance to max of 100m and one computed above (don't want to zoom in too close as doctor approaches)
+        let regionRadius = max(distanceBetweenCoordinates, 100);
+        
+        // update marker position to given coordinates
+        docMarker.coordinate = CLLocationCoordinate2D(latitude: docLocation.latitude, longitude: docLocation.longitude);
         
     }
     
-
 }
